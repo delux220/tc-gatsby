e6e1a033e4c0a032a1b3d9997cbd8baaafec60fa
delux220 <mikey@wigmarket.com>
2021-11-24 15:45:16 -0500

    tailwind postcss stuff

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": () => import("./../../caches/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---cache-caches-gatsby-plugin-offline-app-shell-js" */),
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-article-strapi-article-slug-js": () => import("./../../../src/pages/article/{StrapiArticle.slug}.js" /* webpackChunkName: "component---src-pages-article-strapi-article-slug-js" */),
   "component---src-pages-category-strapi-category-slug-js": () => import("./../../../src/pages/category/{StrapiCategory.slug}.js" /* webpackChunkName: "component---src-pages-category-strapi-category-slug-js" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/dave/gatsby-strapi/frontend/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/dave/gatsby-strapi/frontend/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/dave/gatsby-strapi/frontend/src/pages/404.js")),
   "component---src-pages-article-strapi-article-slug-js": preferDefault(require("/Users/dave/gatsby-strapi/frontend/src/pages/article/{StrapiArticle.slug}.js")),
   "component---src-pages-category-strapi-category-slug-js": preferDefault(require("/Users/dave/gatsby-strapi/frontend/src/pages/category/{StrapiCategory.slug}.js")),
