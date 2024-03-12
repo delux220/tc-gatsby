ea2a8501ad21d7dceed071a571a002a493c4dfed
delux220 <dave.luke@gmail.com>
2024-03-12 17:05:51 -0400

    changes

--- src/pages/v2.js
+++ src/pages/v2.js
@@ -48,10 +48,10 @@ const IndexPage = () => {
         <div className="text-center col-span-3 md:flex items-center h-[50%] md:h-auto">
           <div className="md:h-auto flex justify-between flex-col overflow-hidden bg-[url(https://res.cloudinary.com/meshed-nyc/w_800,c_fill,q_auto/tracey_ai_hi_res_bc8fc1253f_5d37608e22_4b86c8f1aa_ghetjy.jpg)] h-[50vh] md:bg-none bg-contain bg-no-repeat bg-bottom">
             <img src="https://res.cloudinary.com/meshed-nyc/w_800,c_fill,q_auto/tracey_ai_hi_res_bc8fc1253f_5d37608e22_4b86c8f1aa_ghetjy.jpg" className="hidden md:block mx-auto w-[280px] md:w-full md:mb-8"/>
-            <h1 className="font-bold -mt-4 md:mt-0 text-5xl md:text-6xl uppercase font-unica pb-0 mb-0 text-white mt-10 md:mt-0 mb-3">{data.strapiHero.Title}</h1>
+            <h1 className="font-bold -mt-6 md:mt-0 text-5xl md:text-6xl uppercase font-unica pb-0 mb-0 text-white mt-10 md:mt-0 mb-3">{data.strapiHero.Title}</h1>
             <div>
-            <strong className="text-white text-sm font-unica mb-4 uppercase text-center block md:hidden">Follow me on Social Media</strong>
-            <div className="flex items-center justify-between px-4 md:px-0 md:justify-center md:space-x-10 mb-4">
+            <strong className="text-white text-sm font-unica mb-2 uppercase text-center block md:hidden">Follow me on Social Media</strong>
+            <div className="flex items-center justify-between px-4 md:px-0 md:justify-center md:space-x-10 mb-2">
               <a href={data.strapiSocial.Facebook} className="">
                 <img src="/facebook.png" className="w-5 mx-auto fill-teal-600 text-teal-600"/>
               </a>
